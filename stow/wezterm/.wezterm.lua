dcecb4ab643cd670bcd571f9a4347de840e8511d
Will C. Forte <willcforte@gmail.com>
2026-05-21 16:51:11 -0500

    feat: add Gruvbox color scheme to WezTerm and Starship prompt to install

--- stow/wezterm/.wezterm.lua
+++ stow/wezterm/.wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+config.color_scheme = 'GruvboxDark'
 config.font = wezterm.font 'Iosevka Nerd Font'
 config.font_size = 20
 config.initial_cols = 100
