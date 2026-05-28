97e599807a988d3f6c206adb186f325a118ffb52
Will C. Forte <willcforte@gmail.com>
2026-05-27 21:33:23 -0500

    docs(memory): record It1 energy ablation plan + clean stale follow-up
    - project_persona_rl_power_model: ablation arms, 5x weight rule, launch tooling
    - tasks-running-list: drop invalid post-ablation spine_pitch reminder (confirmed 2 actuators)
    - vn200-utb-pcba, settings.json, wezterm: incidental tweaks bundled in this sync

--- stow/wezterm/.wezterm.lua
+++ stow/wezterm/.wezterm.lua
@@ -3,7 +3,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'GruvboxDark'
 config.font = wezterm.font 'Iosevka Nerd Font'
-config.font_size = 20
+config.font_size = 18
 config.initial_cols = 100
 config.initial_rows = 24
 
