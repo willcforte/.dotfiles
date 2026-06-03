3ddd3fba01b65a3cfb985a6eb4ffa561e733f61d
Will C. Forte <willcforte@gmail.com>
2026-06-03 13:06:13 -0500

    fix(nix): pin home-manager to release-25.05 to match nixpkgs pin

--- stow/nix/.config/home-manager/update-config.sh
+++ stow/nix/.config/home-manager/update-config.sh
@@ -1,1 +1,1 @@
-nix run home-manager/master -- switch --flake "$HOME/.dotfiles/stow/nix/.config/home-manager"
+nix run home-manager/release-25.05 -- switch --flake "$HOME/.dotfiles/stow/nix/.config/home-manager"
