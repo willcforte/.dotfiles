95a2e0785fc05a52ebfd2113e98db58913f1def8
Will C. Forte <willcforte@gmail.com>
2026-06-03 23:09:32 -0400

    feat(gnome): use system font in terminal profile

--- stow/nix/.config/home-manager/gnome/terminal.nix
+++ stow/nix/.config/home-manager/gnome/terminal.nix
@@ -12,8 +12,7 @@ with lib.hm.gvariant;
 
     "org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9" = {
       default-size-columns = 80;
-      font = "Iosevka Nerd Font 18";
-      use-system-font = false;
+      use-system-font = true;
       visible-name = "Will";
     };
 
