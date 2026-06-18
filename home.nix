ccf0a90cb3b8410712e1e368b96e76d8b30f7379
Will C. Forte <willcforte@gmail.com>
2026-06-17 23:29:33 -0500

    chore: set git init.defaultBranch to main

--- home.nix
+++ home.nix
@@ -184,6 +184,7 @@ in {
     settings = {
       user.name = "Will C. Forte";
       user.email = "willcforte@gmail.com";
+      init.defaultBranch = "main";
       push.autoSetupRemote = true;
       # Clear any inherited helper (empty string), then delegate to gh.
       credential."https://github.com".helper = [ "" "!gh auth git-credential" ];
