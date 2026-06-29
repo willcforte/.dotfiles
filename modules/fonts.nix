4f272e3fa16632e33464dcbaca92eeca976e0580
Will C. Forte <willcforte@gmail.com>
2026-06-29 10:01:40 -0500

    feat: add fonts.nix with SF Pro

---
+++ modules/fonts.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+let
+  # SF Pro is proprietary (Apple) and not in nixpkgs; build from the upstream
+  # repo of .otf/.ttf files. Bump rev+hash with nix-prefetch-github.
+  sf-pro-fonts = pkgs.stdenvNoCC.mkDerivation {
+    pname = "sf-pro-fonts";
+    version = "0-unstable-8bfea09";
+    src = pkgs.fetchFromGitHub {
+      owner = "sahibjotsaggu";
+      repo = "San-Francisco-Pro-Fonts";
+      rev = "8bfea09aa6f1139479f80358b2e1e5c6dc991a58";
+      hash = "sha256-mAXExj8n8gFHq19HfGy4UOJYKVGPYgarGd/04kUIqX4=";
+    };
+    installPhase = ''
+      runHook preInstall
+      install -Dm644 *.otf -t $out/share/fonts/opentype
+      install -Dm644 *.ttf -t $out/share/fonts/truetype
+      runHook postInstall
+    '';
+    meta.description = "San Francisco Pro fonts, packaged from sahibjotsaggu/San-Francisco-Pro-Fonts";
+  };
+in {
+  home.packages = [
+    sf-pro-fonts                                    # SF Pro Display / Text (GNOME)
+    pkgs.nerd-fonts.iosevka                         # "Iosevka Nerd Font" (VSCode)
+    (pkgs.iosevka-bin.override { variant = "SGr-IosevkaTermSS18"; })  # IosevkaTerm SS18 (GNOME mono)
+  ];
+
+  # Make fontconfig see fonts from home.packages on non-NixOS.
+  fonts.fontconfig.enable = true;
+}
