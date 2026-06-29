7eeb299200cb45e973e398bb9c51ab24f8a1cadd
Will C. Forte <willcforte@gmail.com>
2026-06-28 19:18:26 -0500

    refactor: specify all dconf into gnome.nix

--- modules/gnome/gnome.nix
+++ modules/gnome/gnome.nix
@@ -5,6 +5,16 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      clock-format = "12h";
+      clock-show-date = true;
+      clock-show-weekday = true;
+      color-scheme = "light";
+      enable-hot-corners = false;
+      gtk-theme = "Yaru-dark";
+      icon-theme = "Yaru";
+    };
+
     "org/gnome/shell/extensions/dash-to-dock" = {
       dash-max-icon-size = 46;
       dock-fixed = true;
@@ -30,5 +40,15 @@ with lib.hm.gvariant;
       tiling-popup-all-workspace = true;
     };
 
+    "org/gnome/terminal/legacy/profiles:" = {
+      default = "b1dcc9dd-5262-4d8d-a863-c897e6d979b9";
+      list = [ "b1dcc9dd-5262-4d8d-a863-c897e6d979b9" ];
+    };
+
+    "org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9" = {
+      default-size-columns = 80;
+      use-system-font = true;
+      visible-name = "Will";
+    };
   };
 }
