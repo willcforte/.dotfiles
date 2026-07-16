3275b3e78f31fb1170897414a90b5239183107c3
Will C. Forte <willcforte@gmail.com>
2026-07-16 12:36:52 -0500

    feat: starship with jj support

--- modules/shell/common.nix
+++ modules/shell/common.nix
@@ -2,6 +2,7 @@
   # Shell integrations shared across platforms (zsh on both Linux and Darwin).
   programs.starship = {
     enable = true;
+    enableZshIntegration = true;
     settings.hostname = {
       ssh_only = false;
       aliases = {
@@ -10,7 +11,23 @@
         "will-mbp" = "mbp";
       };
     };
+
+    # 1. Add your custom JJ module into your prompt layout string
+    settings.format = "$all\${custom.jj}$character";
+
+    # 2. Define the custom JJ module block
+    settings.custom.jj = {
+      command = "prompt";
+      format = "on [$output](bold purple) ";
+      ignore_timeout = true;
+      # Calls the 'jj-starship' binary in the shell 
+      shell = ["jj-starship" "--ignore-working-copy" "starship"];
+      use_stdin = false;
+      # Only triggers the command if a .jj directory exists
+      when = "test -d .jj";
+    };
   };
+
   programs.zoxide.enable = true;
   programs.fzf = {
     enable = true;
