afd66d4b18ac1ce53946f8be473a3b6aa3553ef1
Will C. Forte <willcforte@gmail.com>
2026-07-19 16:38:50 -0500

    feat(install): collect nix garbage after each home-manager switch

--- install.sh
+++ install.sh
@@ -87,6 +87,7 @@ else
   nix run home-manager/master -- switch -b backup --flake "$DOTFILES"
 fi
 export PATH="$HOME/.nix-profile/bin:$PATH"
+nix-collect-garbage -d
 
 #-----------------------------------------------------------
 # 4b. Register nix zsh in /etc/shells and set it as the login shell.
